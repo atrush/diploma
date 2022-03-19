ce071e494cccaf9cb3e10fcb838c2e26b3de8248
atrush <anton.trush@gmail.com>
2022-03-20 00:16:51 +0300

    add cfg and psql storage in main

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"context"
 	"github.com/atrush/diploma.git/api"
+	"github.com/atrush/diploma.git/pkg"
+	"github.com/atrush/diploma.git/storage/psql"
 	"log"
 	"os"
 	"os/signal"
 )
 
 func main() {
-	server, err := api.NewServer()
+
+	cfg, err := pkg.NewConfig()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	//fmt.Printf("config dsn: %v" \n, cfg.DatabaseDSN)
+
+	db, err := psql.NewStorage(cfg.DatabaseDSN)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	server, err := api.NewServer(cfg, db)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
